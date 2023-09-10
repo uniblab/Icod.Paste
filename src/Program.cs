6f1d0c8aec8039603c50c3c12b1ca0314c769c6e
uniblab <uniblab@hotmail.com>
2023-09-10 18:58:18 -0400

    corrected copyright & license

--- src/Program.cs
+++ src/Program.cs
@@ -79,26 +79,19 @@ namespace Icod.Paste {
 		private static void PrintCopyright() {
 			var copy = new System.String[] {
 				"Paste.exe returns text from the clipboard.",
-				"",
 				"Copyright( C ) 2023 Timothy J. Bruce",
 				"",
 				"This program is free software: you can redistribute it and / or modify",
 				"it under the terms of the GNU General Public License as published by",
 				"the Free Software Foundation, either version 3 of the License, or",
 				"( at your option ) any later version.",
-				"",
-				"",
 				"This program is distributed in the hope that it will be useful,",
 				"but WITHOUT ANY WARRANTY; without even the implied warranty of",
-				"",
-				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the",
-				"",
+				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
 				"GNU General Public License for more details.",
 				"",
-				"",
 				"You should have received a copy of the GNU General Public License",
-				"",
-				"along with this program.If not, see < https://www.gnu.org/licenses/>."
+				"along with this program.If not, see <https://www.gnu.org/licenses/>."
 			};
 			foreach ( var line in copy ) {
 				System.Console.WriteLine( line );
